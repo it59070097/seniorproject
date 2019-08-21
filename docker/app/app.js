6bf704798522f6ee954a90d585739f083f48c2cb
pun sutanchaiyanonta <puncud51@puns-Mac-mini.it.kmitl.ac.th>
2019-08-21 19:17:42 +0700

    connect database

--- docker/app/app.js
+++ docker/app/app.js
@@ -1,8 +1,18 @@
-const express = require('express');
+const express = require('express')
+const pgp = require('pg-promise')()
 const app = express()
+const db = pgp('postgres://postgres:example@db:5432/postgres')
 
 app.get('/', (req, res) => res.send('hello pun'));
 
 app.listen(3000, () => {
     console.log('My Rest API running on port 3000!');
 } )
+
+db.one('SELECT * FROM public.cars')
+  .then(function (data) {
+    console.log('DATA:', data.value)
+  })
+  .catch(function (error) {
+    console.log('ERROR:', error)
+  })
